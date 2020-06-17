6331047101b1c5f28173be4bb0d7857f908eeb36
tatikaze <43057959+tatikaze@users.noreply.github.com>
2020-06-17 10:32:45 +0900

    [change] echo instance name
    Co-authored-by: Akira Shinohara <k017c1067@it-neec.jp>

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
+	app := echo.New()
+	app.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!!")
 	})
-	e.Logger.Fatal(e.Start(":3000"))
+	app.Logger.Fatal(e.Start(":3000"))
 }
