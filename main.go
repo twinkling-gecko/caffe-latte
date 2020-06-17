3c0ee469ead83ce8efef573346c6f45d76ccc564
tatikaze <k017c1356@it-neec.jp>
2020-06-17 09:39:07 +0900

    [clean]

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-  e := echo.New()
-  e.GET("/", func(c echo.Context) error {
-    return c.String(http.StatusOK, "Hello World!!")
-  })
-  e.Logger.Fatal(e.Start(":3000"))
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World!!")
+	})
+	e.Logger.Fatal(e.Start(":3000"))
 }
